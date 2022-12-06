3dc66edd68fd9fab3238da54d63bbcae079617af
Marek Zelinka <mzelinka17@gmail.com>
2022-12-06 19:18:16 +0100

    fix alert

--- app/components/alert.tsx
+++ app/components/alert.tsx
@@ -9,10 +9,7 @@ export function Alert(props: { title: string; children?: string }) {
       <div className="space-y-2 ">
         <h3 className="text-sm font-medium text-red-800">{title}</h3>
         {children && (
-          <p className="max-w-prose text-sm text-red-700">
-            We can't find the contact you're looking for. Please check your URL.
-            Has the contact been deleted?
-          </p>
+          <div className="max-w-prose text-sm text-red-700">{children}</div>
         )}
       </div>
     </div>
