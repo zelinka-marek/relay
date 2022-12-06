50d1bc1cd6337ecd59a9b5b9c7c32ce9f8f090e8
Marek Zelinka <mzelinka17@gmail.com>
2022-12-06 09:11:15 +0100

    init

---
+++ app/root.tsx
@@ -0,0 +1,39 @@
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+import tailwindStylesheetUrl from "./styles/tailwind.css";
+
+export const meta: MetaFunction = () => {
+  return {
+    charset: "utf-8",
+    title: "Relay",
+    viewport: "width=device-width,initial-scale=1",
+  };
+};
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: tailwindStylesheetUrl }];
+};
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
---
+++ app/routes/index.tsx
@@ -0,0 +1,7 @@
+export default function IndexRoute() {
+  return (
+    <div>
+      <h1>Welcome to Relay</h1>
+    </div>
+  );
+}
