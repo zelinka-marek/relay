448eedf780b7f2615886af1f15fe0839c25600b8
Marek Zelinka <mzelinka17@gmail.com>
2022-12-07 13:41:42 +0100

    delete note

--- app/routes/__app/contacts/$contactId/notes.tsx
+++ app/routes/__app/contacts/$contactId/notes.tsx
@@ -1,9 +1,12 @@
-import type { LoaderArgs } from "@remix-run/node";
+import type { ActionArgs, LoaderArgs, SerializeFrom } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { Form, useLoaderData } from "@remix-run/react";
+import { Form, useFetcher, useLoaderData } from "@remix-run/react";
 import { formatDistanceToNowStrict, format } from "date-fns";
 import invariant from "tiny-invariant";
 import { prisma } from "~/db.server";
+
+type LoaderData = SerializeFrom<typeof loader>;
+type LoaderNote = LoaderData["notes"][number];
 
 export async function loader({ params }: LoaderArgs) {
   invariant(params.contactId, "contactId is missing");
@@ -25,6 +28,68 @@ export async function loader({ params }: LoaderArgs) {
   return json({ notes });
 }
 
+export async function action({ request }: ActionArgs) {
+  const formData = await request.formData();
+
+  const noteId = formData.get("noteId");
+  invariant(typeof noteId === "string", "noteId is missing");
+
+  const note = await prisma.note.findUnique({
+    where: { id: noteId },
+    select: { id: true },
+  });
+  if (!note) {
+    throw json("Note not found", { status: 404 });
+  }
+
+  const deletedNote = await prisma.note.delete({ where: { id: note.id } });
+
+  return json({ deletedNote });
+}
+
+function NoteItem(props: { note: LoaderNote }) {
+  const { note } = props;
+
+  const deleteFetcher = useFetcher();
+
+  return (
+    <li key={note.id} className="py-5">
+      <p className="text-sm font-medium text-gray-800">{note.title}</p>
+      <p className="text-sm text-gray-500">
+        <time dateTime={note.createdAt}>
+          {format(new Date(note.createdAt), "MMM d")},{" "}
+          {formatDistanceToNowStrict(new Date(note.createdAt), {
+            addSuffix: true,
+            unit: "day",
+            roundingMethod: "ceil",
+          })}
+        </time>
+      </p>
+      <p className="mt-1 max-w-prose text-sm text-gray-600">{note.body}</p>
+      <div className="mt-2 flex gap-2">
+        <Form action={`${note.id}/edit`} className="inline-flex">
+          <button
+            type="submit"
+            className="rounded-md bg-gray-50 text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+          >
+            Edit
+          </button>
+        </Form>
+        <span className="text-gray-500">·</span>
+        <deleteFetcher.Form method="post" className="inline-flex">
+          <input type="hidden" name="noteId" value={note.id} />
+          <button
+            type="submit"
+            className="rounded-md bg-gray-50 text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+          >
+            Delete
+          </button>
+        </deleteFetcher.Form>
+      </div>
+    </li>
+  );
+}
+
 export default function ContactNotesRoute() {
   const { notes } = useLoaderData<typeof loader>();
 
@@ -41,41 +106,7 @@ export default function ContactNotesRoute() {
       <div className="flow-root">
         <ul role="list" className="-my-5 divide-y divide-gray-200">
           {notes.map((note) => (
-            <li key={note.id} className="py-5">
-              <p className="text-sm font-medium text-gray-800">{note.title}</p>
-              <p className="text-sm text-gray-500">
-                <time dateTime={note.createdAt}>
-                  {format(new Date(note.createdAt), "MMM d")},{" "}
-                  {formatDistanceToNowStrict(new Date(note.createdAt), {
-                    addSuffix: true,
-                    unit: "day",
-                    roundingMethod: "ceil",
-                  })}
-                </time>
-              </p>
-              <p className="mt-1 max-w-prose text-sm text-gray-600">
-                {note.body}
-              </p>
-              <div className="mt-2 flex gap-2">
-                <Form action={`${note.id}/edit`} className="inline-flex">
-                  <button
-                    type="submit"
-                    className="rounded-md bg-gray-50 text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                  >
-                    Edit
-                  </button>
-                </Form>
-                <span className="text-gray-500">·</span>
-                <Form method="post" className="inline-flex">
-                  <button
-                    type="submit"
-                    className="rounded-md bg-gray-50 text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                  >
-                    Delete
-                  </button>
-                </Form>
-              </div>
-            </li>
+            <NoteItem key={note.id} note={note} />
           ))}
         </ul>
       </div>
